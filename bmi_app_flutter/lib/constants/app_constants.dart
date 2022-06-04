b120246dfc2c1fd14715cae219925fa7378a252c
shreya <shettarshreya@gmail.com>
2022-06-04 18:16:47 +0530

    initial commit

---
+++ bmi_app_flutter/lib/constants/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHex =HexColor("#FFF8F3");
+final Color accentHex =HexColor("#072227");
+
---
+++ bmi_app_flutter/lib/widgets/rightbar.dart
@@ -0,0 +1,28 @@
+import 'package:bmi_app_flutter/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class RightBar extends StatelessWidget{
+  final double barWidth;
+
+  const RightBar({required this.barWidth}) ;
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children :[
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ), //BorderRadius
+              color: accentHex
+          ),
+        )
+      ],
+    );
+
+  }
+}
